50fc47b5f87f3617d70d6555992adde68713cb69
Wardega Dariusz <dariusz.wardega@bwigroup.com>
2019-07-24 13:07:59 +0200

    Adding main menu of application

--- src/pl/dwardega/calculator/GUI/Console/Menu.java
+++ src/pl/dwardega/calculator/GUI/Console/Menu.java
@@ -1,4 +1,64 @@
 package pl.dwardega.calculator.GUI.Console;
 
+import java.util.Scanner;
+
 public class Menu {
+    public static void mainMenu(){
+        clearScreen();
+        Scanner optionChooser = new Scanner(System.in);
+
+        // this condition is changed when user want to exit menu
+        // (it's breaking the while loop)
+        boolean stillWorking = true;
+
+        while(stillWorking){
+            System.out.println(mainMenuStatement());
+            int option = optionChooser.hasNextInt() ? optionChooser.nextInt():-1;
+            switch (option) {
+                case 1:
+                    // menu for figures
+                    clearScreen();
+                    System.out.println("Figures");
+                    break;
+                case 2:
+                    // menu for prism
+                    clearScreen();
+                    System.out.println("Prism");
+                    break;
+                case 0:
+                    clearScreen();
+                    System.out.println(quitStatement());
+                    stillWorking = false;
+                    break;
+                default:
+                    clearScreen();
+                    System.out.println(wrongChoiceStatement());
+                    // ??? working correctly with that
+                    optionChooser.nextLine();
+                    break;
+            }
+        }
+    }
+
+    private static void clearScreen(){
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+    }
+
+    private static String wrongChoiceStatement(){
+        return "\nWrong choice! Please try again!\n";
+    }
+
+    private static String  mainMenuStatement(){
+        return "--- GEOMETRIC CALCULATOR ---"+
+                "\nChoose category: " +
+                "\n1. Flat figures" +
+                "\n2. Spatial figures" +
+                "\n0. QUIT" +
+                "\n[Input correct number]";
+    }
+
+    private static String quitStatement(){
+        return "Thanks for working. Bye bye :)";
+    }
 }
--- src/pl/dwardega/calculator/Main.java
+++ src/pl/dwardega/calculator/Main.java
@@ -1,8 +1,15 @@
 package pl.dwardega.calculator;
 
+import pl.dwardega.calculator.GUI.Console.Menu;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Menu.mainMenu();
+        try{
+            Thread.sleep(2000);
+        } catch (InterruptedException ex){}
+        System.exit(-1);
     }
 }
